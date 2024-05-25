6c48306da8d6064935875e4603fd070da9ea1959
Naveen Hettiwaththa <naveenhettiwaththa@gmail.com>
2024-05-25 17:15:07 +0530

    added header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,4 @@
-import Image from "next/image";
-import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import MenuIcon from '@mui/icons-material/Menu';
-import { Input } from "@/components/ui/input";
+import Header from "@/components/home/Header";
 
 export default function Home() {
   return (
@@ -12,36 +8,9 @@ export default function Home() {
         <label className="text-sm">Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</label>
       </div>
 
-      <div style={{ border: '1px solid rgba(0,0,0,0.2)' }} className="w-full flex justify-between pt-10 pb-5 px-24">
+      <Header />
 
-        <div className="my-auto">
-          <h1 className="text-xl font-bold">Paper Trail</h1>
-        </div>
-
-        <div className="hidden my-auto lg:flex gap-10 justify-between">
-          <label className=" underline cursor-pointer">Home</label>
-          <label className="cursor-pointer">Contact</label>
-          <label className="cursor-pointer">About</label>
-          <label className="cursor-pointer">Sign Up</label>
-        </div>
-
-
-        <div className="my-auto flex gap-5">
-
-          <div className="hidden my-auto lg:flex ">
-            <Input type="text" placeholder="What are you looking for?" />
-          </div>
-
-          <div className="flex my-auto gap-5 ">
-            <FavoriteBorderIcon className="cursor-pointer" />
-            <ShoppingCartOutlinedIcon className="cursor-pointer" />
-            <MenuIcon className="ml-4 flex cursor-pointer" />
-          </div>
-
-        </div>
-
-      </div>
-
+      
 
     </main>
   );
---
+++ src/components/home/Header.tsx
@@ -0,0 +1,39 @@
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Input } from "@/components/ui/input";
+
+
+export default function Header() {
+    return (
+        <div style={{ border: '1px solid rgba(0,0,0,0.2)' }} className="w-full flex justify-between pt-10 pb-5 px-24">
+
+            <div className="my-auto">
+                <h1 className="text-xl font-bold">Paper Trail</h1>
+            </div>
+
+            <div className="hidden my-auto lg:flex gap-10 justify-between">
+                <label className=" underline cursor-pointer">Home</label>
+                <label className="cursor-pointer">Contact</label>
+                <label className="cursor-pointer">About</label>
+                <label className="cursor-pointer">Sign Up</label>
+            </div>
+
+
+            <div className="my-auto flex gap-5">
+
+                <div className="hidden my-auto lg:flex ">
+                    <Input type="text" placeholder="What are you looking for?" />
+                </div>
+
+                <div className="flex my-auto gap-5 ">
+                    <FavoriteBorderIcon className="cursor-pointer" />
+                    <ShoppingCartOutlinedIcon className="cursor-pointer" />
+                    <MenuIcon className="ml-4 flex cursor-pointer" />
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
