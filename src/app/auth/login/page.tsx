c0b5eec531f2ae3eb78fa1342893e42ff6d2252c
Naveen Hettiwaththa <naveenhettiwaththa@gmail.com>
2024-05-30 00:31:37 +0530

    added login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,11 +1,10 @@
-"use client"
 import Footer from "@/components/home/Footer";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import Image from "next/image";
 import GoogleIcon from '@mui/icons-material/Google';
 
 
-export default function Register() {
+export default function Login() {
     return (
         <div className="flex flex-col w-full">
 
@@ -21,30 +20,24 @@ export default function Register() {
 
                     <div className="flex flex-col w-full max-w-[400px]">
 
-                        <h1 className="text-3xl font-semibold">Create an account</h1>
+                        <h1 className="text-3xl font-semibold">Log in to Exclusive</h1>
 
                         <label className="text-sm mt-3">Enter your details below</label>
 
                         <div className="pb-2 flex flex-col gap-10 mt-8">
 
                             <div style={{ borderBottom: '1px solid rgba(0,0,0,0.2)' }} className="pb-3 flex w-full ">
-                                <input className="w-full" style={{ outline: 'none' }} type="text" placeholder="Name" />
-                            </div>
-
-                            <div style={{ borderBottom: '1px solid rgba(0,0,0,0.2)' }} className="pb-3 flex w-full ">
                                 <input className="w-full" style={{ outline: 'none' }} type="text" placeholder="Email or Phone Number" />
                             </div>
 
                             <div style={{ borderBottom: '1px solid rgba(0,0,0,0.2)' }} className="pb-3 flex w-full ">
-                                <input className="w-full" onChange={(e) => console.log("key is -> ", e.target.value)} style={{ outline: 'none' }} type="password" placeholder="Password" />
+                                <input className="w-full" style={{ outline: 'none' }} type="password" placeholder="Password" />
                             </div>
 
-                            <div className="flex flex-col gap-5 w-full">
-                                <button className="bg-[#DB4444] rounded-md px-5 justify-center items-center text-white text-sm p-2 w-full ">Create Account</button>
-                                <button style={{ border: '1px solid rgba(0,0,0,0.2)' }} className="rounded-md gap-2 font-semibold px-5 justify-center items-center text-black text-sm p-2 w-full flex ">
-                                    <GoogleIcon className="text-sm" />
-                                    Sign up with google</button>
-                                <center><label>Already have account? <span className=" cursor-pointer underline">Log in</span></label></center>
+                            <div className="flex flex-row justify-between w-full my-auto">
+                                <button className="bg-[#DB4444] rounded-md px-5 justify-center items-center text-white text-sm p-2 w-full max-w-[100px] my-auto">Log In</button>
+
+                                <center><label className="text-[#DB4444] my-auto font-semibold cursor-pointer">Forget password?</label></center>
                             </div>
 
 
