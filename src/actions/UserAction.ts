c4cc8e723d39ec239c2c51ed79770c927a551cca
Naveen Hettiwaththa <naveenhettiwaththa@gmail.com>
2024-06-04 22:37:18 +0530

    added bug fixes

--- src/actions/UserAction.ts
+++ src/actions/UserAction.ts
@@ -4,17 +4,20 @@ import admin from "@/firebase/admin"
 import { User } from "@/types/types"
 
 export const handleUserSave = async (userData:string) => {
-    const data:User = JSON.parse(userData)
-
-    const q = admin.app().firestore().collection('user').where('email' , '==' , data.email)
     
-    if ((await q.get()).docs.length > 0){
+    const data:User = JSON.parse(userData)
+    console.log('ran 1 => ' , admin.apps.length)
+
+    const q = admin.firestore().collection('user').where('email' , '==' , data.email)
+    const results = await q.get()
+    
+    if(results.docs.length > 0){
         return {
             success : false,
             message : 'Already has a user account for this email.'
         }
     }
-
+        
     await admin.app().firestore().collection('user').doc(data.uid).set(data)
 
     return {
--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -1,35 +1,25 @@
-// src/firebase/admin.ts
 import admin from 'firebase-admin';
-// var serviceAccount = require("../paper-trail.json");
+var serviceAccount = require("../paper-trail.json");
+
+function formatPrivateKey(key:string){
+    return key.replace(/\\n/g, "\n")
+}
 
 
-// if (admin.apps == null){
-//     admin.initializeApp({
-//         credential: admin.credential.cert(serviceAccount)
-//     });
-// }
+if (admin.apps.length == 0){
 
+    const privateKey = formatPrivateKey(process.env.FIREBASE_PRIVATE_KEY as string)
 
-if (!admin.apps.length) {
     admin.initializeApp({
-      credential: admin.credential.cert({
-        // @ts-ignore
-        type: process.env.FIREBASE_TYPE,
-        project_id: process.env.FIREBASE_PROJECT_ID,
-        private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-        // @ts-ignore
-        private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-        client_email: process.env.FIREBASE_CLIENT_EMAIL,
-        client_id: process.env.FIREBASE_CLIENT_ID,
-        auth_uri: process.env.FIREBASE_AUTH_URI,
-        token_uri: process.env.FIREBASE_TOKEN_URI,
-        auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-        client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-      }),
-      databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
+        credential: admin.credential.cert({
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey,
+            projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
+        }),
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET
     });
-  }
 
-
+}
 
 export default admin
