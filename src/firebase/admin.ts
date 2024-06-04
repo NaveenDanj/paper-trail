cb587fa384490a1ea6c57886b8be7a46edb50be8
Naveen Hettiwaththa <naveenhettiwaththa@gmail.com>
2024-06-04 17:03:32 +0530

    added env changes

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -1,11 +1,13 @@
 // src/firebase/admin.ts
 import admin from 'firebase-admin';
-var serviceAccount = require("../paper-trail.json");
-
 
 if (admin.apps == null){
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount)
+        credential: admin.credential.cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')+'',
+        }),
     });
 }
 
